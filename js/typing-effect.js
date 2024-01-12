c373ead835985dc34288913a79633278cde01c2e
Pablo Rossoni <pablorossoni107@outlook.com>
2024-01-11 23:45:01 -0300

    Adicionando o arquivo que contem o efeito de digitação

---
+++ js/typing-effect.js
@@ -0,0 +1,9 @@
+
+    let typed = new Typed ('#typed', {
+        strings: [
+            "O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan."
+        ],
+        typeSpeed: 30,
+        showCursor: false,
+    });
+
